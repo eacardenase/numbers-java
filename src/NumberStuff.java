e48bb12a92d8e737a8cbb9d27a5642306bbd9c62
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-26 17:28:22 -0500

    feat: simple number types

---
+++ src/NumberStuff.java
@@ -0,0 +1,13 @@
+public class NumberStuff {
+    public static void main(String[] args) {
+        boolean myFlag = true; // 0 or 1
+        byte myByte = 13; // -128 to 127
+        short myShort = 45; // -32,768 to 32,767
+        char myChar = 'ñ';
+        char myA = 'A';
+
+        System.out.println(myByte);
+        System.out.println(myFlag);
+
+    }
+}
