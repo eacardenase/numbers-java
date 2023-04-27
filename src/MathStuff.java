a134946ebdc6ab20a932662fbd9ed0684c1503d4
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-26 20:43:57 -0500

    feat: simple math operations

---
+++ src/MathStuff.java
@@ -0,0 +1,35 @@
+public class MathStuff {
+    public static void main(String[] args) {
+//        System.out.println(3 + 5);
+//        System.out.println(10 - 7);
+//        System.out.println(7 - 10);
+//        System.out.println(7 * 4);
+//        System.out.println(8 / 4);
+//        System.out.println(7 / 4); // parsed to integer
+//        System.out.println(7f / 4); // 1.75
+//        System.out.println((double) 7 / 4); // type casting
+
+//        double num1 = 7;
+//        float num2 = 4;
+//
+//        System.out.println(num1 / num2);
+
+//        System.out.println(3d * 5); // d makes an int a double
+
+//        System.out.println(2 + 3 * 5); // 17
+//        System.out.println((2 + 3) * 5); // 25
+
+//        System.out.println(10 % 2);
+
+        int x = 1;
+        x += 1;
+        x -= 1;
+        x *= 1;
+        x /= 1;
+
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x++);
+
+    }
+}
