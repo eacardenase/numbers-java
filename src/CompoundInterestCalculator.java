a06c6b723717f78a8fe10376198ebfa777d4a14b
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-27 14:15:35 -0500

    feat: add number formatter and change calculate method to be static

--- src/CompoundInterestCalculator.java
+++ src/CompoundInterestCalculator.java
@@ -1,35 +1,32 @@
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public class CompoundInterestCalculator {
 
-    BigDecimal principal;
-    BigDecimal rate;
-    int period;
-    BigDecimal contribution;
+    private static final NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormatter = NumberFormat.getPercentInstance();
 
-    public CompoundInterestCalculator(String principal, String rate, int period, String contribution) {
-        this.principal = new BigDecimal(principal);
-        this.rate = new BigDecimal(rate);
-        this.period = period;
-        this.contribution = new BigDecimal(contribution);
-    }
-
-    public BigDecimal calculate() {
+    public static BigDecimal calculate(String principal, String rate, int period, String contribution) throws ParseException {
         // Balance (Y) = P(1 + r)^Y + c[ ((1 + r)^Y - 1) / r]
 
-        BigDecimal a = BigDecimal.ONE.add(this.rate).pow(this.period); // (1 + r)^Y
+        String rateAsPercent = percentFormatter.parse(rate).toString();
+
+        BigDecimal a = BigDecimal.ONE.add(new BigDecimal(rateAsPercent)).pow(period); // (1 + r)^Y
         BigDecimal b = a.subtract(BigDecimal.ONE); // (1 + r)^Y - 1
-        BigDecimal c = b.divide(this.rate); // ((1 + r)^Y - 1) / r
-        BigDecimal d = c.multiply(this.contribution); // c[ ((1 + r)^Y - 1) / r]
-        BigDecimal e = this.principal.multiply(a); // P(1 + r)^Y
+        BigDecimal c = b.divide(new BigDecimal(rateAsPercent)); // ((1 + r)^Y - 1) / r
+        BigDecimal d = c.multiply(new BigDecimal((moneyFormatter.parse(contribution).toString()))); // c[ ((1 + r)^Y - 1) / r]
+        BigDecimal e = a.multiply(new BigDecimal(moneyFormatter.parse(principal).toString())); // P(1 + r)^Y
         BigDecimal f = e.add(d); // P(1 + r)^Y + c[ ((1 + r)^Y - 1) / r]
 
         return f;
     }
 
-    public static void main(String[] args) {
-        CompoundInterestCalculator calculator = new CompoundInterestCalculator("10000.00", "0.08", 10, "1000");
+    public static void main(String[] args) throws ParseException {
+        BigDecimal compoundInterest = CompoundInterestCalculator.calculate("$25,300", "8%", 10, "$7,500");
 
-        System.out.println(calculator.calculate());
+//        System.out.println(compoundInterest);
+        System.out.println(CompoundInterestCalculator.moneyFormatter.format(compoundInterest)); // $36,075.81
     }
 }
